c21a373e073943585d136731a62edac1e74788fa
Agata <agata@swistowski.org>
2018-11-23 12:50:22 +0000

    first commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+                <div class="bottomMenu">
+					<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
+                </div>
+				<div class="site-info">
+					Brought to you by Agata Swistowska <a href="<?php echo esc_url( 'https://redacademy.com/' ); ?>">
+                        RED Academy
+                    </a>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ page-submit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agataswistowska
+ * Date: 23/11/2018
+ * Time: 12:11
+ */
+get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
